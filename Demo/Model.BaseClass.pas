85b7be385bf9e3e272365b8b975b02432348073e
Ricardo da Rocha <ricardodarocha@outlook.com>
2022-01-25 19:14:27 -0300

    Update Model.BaseClass.pas

--- Demo/Model.BaseClass.pas
+++ Demo/Model.BaseClass.pas
@@ -3,11 +3,11 @@ unit Model.BaseClass;
 interface
 
 uses
-  Vcl.Graphics;
+  Vcl.Graphics, Entidade.Core;
 
 type
 
-  TCar = class
+  TCar = class(TEntidade)
     function Power: Integer; virtual; abstract;
     function Design: string; virtual; abstract;
     function Color: TColor; virtual; abstract;
